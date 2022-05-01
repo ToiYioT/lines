3928cfa6b4ba80fa70540b1a84ef51a1985d1795
= <toiyiot@gmail.com>
2022-05-01 12:37:56 +0300

    Add undo history button to sliders

--- src/components/SliderControl.tsx
+++ src/components/SliderControl.tsx
@@ -1,7 +1,7 @@
 
 import { ActionIcon, Slider } from '@mantine/core';
-import { Exchange } from 'tabler-icons-react';
-import React from 'react'
+import { ArrowBackUp, Exchange } from 'tabler-icons-react';
+import React, { useRef } from 'react'
 
 type Props = {
     name: string
@@ -17,7 +17,36 @@ type Props = {
 
 export default function SliderControl({ name, min, max, step, value, onChange, resetValue }: Props) {
 
+    const historyValues = useRef<number[]>([]);
+    const startValue = useRef<number>(value);
+    const defaultValue = useRef<number>(value);
+
     const resetButton = resetValue != null;
+
+    function undo() {
+
+        const lastValue = historyValues.current.pop();
+        if (lastValue) {
+            onChange(lastValue);
+            startValue.current = lastValue;
+        } else {
+            onChange(defaultValue.current);
+            startValue.current = defaultValue.current;
+        }
+    }
+
+    function onChangeEnd(endValue: number) {
+        historyValues.current.push(startValue.current);
+        startValue.current = endValue;
+
+    }
+
+    function reset() {
+        onChange(resetValue!);
+
+        historyValues.current.push(startValue.current);
+        startValue.current = resetValue!;
+    }
 
     return (
         <div className="slider-section-container">
@@ -33,12 +62,19 @@ export default function SliderControl({ name, min, max, step, value, onChange, r
                         step={step}
                         value={value}
                         onChange={onChange}
+
+                        onChangeEnd={onChangeEnd}
                     />
 
                 </div>
                 <div className="buttons-container">
+
+                    <ActionIcon size="sm" onClick={undo} >
+                        <ArrowBackUp strokeWidth={1} size={16} />
+                    </ActionIcon>
+
                     {resetButton &&
-                        <ActionIcon size="sm" onClick={() => onChange(resetValue)} >
+                        <ActionIcon size="sm" onClick={reset} >
                             <Exchange strokeWidth={1} size={16} />
                         </ActionIcon>
                     }

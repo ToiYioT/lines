bc9750705c805ffb54e16848e54850a6111efc89
ToiYioT <toiyiot@gmail.com>
2022-06-14 15:40:31 +0300

    Add buttons to speed up or down all the animations of the linework

--- src/components/AnimationTab.tsx
+++ src/components/AnimationTab.tsx
@@ -1,6 +1,7 @@
 
-import { Switch } from '@mantine/core'
+import { ActionIcon, Switch } from '@mantine/core'
 import React from 'react'
+import { ChevronsLeft, ChevronsRight } from 'tabler-icons-react'
 import { AnimationStates } from '../hooks/useAnimationState'
 import AnimationControl from './AnimationControl'
 
@@ -15,15 +16,45 @@ export default function AnimationTab({
 
     return (
         <div className="animation-tab-container">
-
             <Switch label="Animation On"
                 checked={animationOn}
                 onChange={(event) => setAnimationOn(event.currentTarget.checked)}
             />
+
+            <div className="animation-speed-container">
+                Animation Speed
+                <ActionIcon
+                    disabled={!animationOn}
+                    onClick={() => speedUpAnimations(animationStates, 0.5)}
+                >
+                    <ChevronsLeft />
+                </ActionIcon>
+
+                <ActionIcon
+                    disabled={!animationOn}
+                    onClick={() => speedUpAnimations(animationStates, 2)}
+                >
+                    <ChevronsRight />
+                </ActionIcon>
+
+            </div>
+
 
             <AnimationControl animationStates={animationStates} />
             <AnimationControl animationStates={animationStates} />
         </div>
 
     )
+}
+
+function speedUpAnimations(animationStates: AnimationStates, speedUpBy: number) {
+
+    (Object.keys(animationStates) as (keyof typeof animationStates)[])
+        .forEach((animationKey) => {
+
+            const animation = animationStates[animationKey];
+            if (animation.active) {
+                animation.setSpeed(animation.speed * speedUpBy)
+            }
+        });
 }
